fad8d9d23753c735c61fbd28a71d57ca013718ad
nashitshayan <shayan.nashit@gmail.com>
2022-01-26 12:08:11 +0530

    feat: player input

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,7 +1,7 @@
 
 
 
-const gameBoard = (()=>{
+const gameBoard = ((playerOne, playerTwo)=>{
     const gameBoardDiv = document.querySelector('.gameBoard');
     const clearBoardBtn = document.getElementById('clearBoard');
     const boxDivs = Array.from(gameBoardDiv.children);
@@ -9,17 +9,50 @@ const gameBoard = (()=>{
        undefined, undefined, undefined,
        undefined, undefined, undefined,
        undefined, undefined, undefined ];
+    // const gameBoardArr= [
+    //     'X', 'O', 'X',
+    //     'O', 'X', 'O',
+    //     'X', 'O', 'X',
+    // ]
     let isX= false;
+    
+    const winnerCheck= ()=>{
+        const threeInARow = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        
+       for(let combo of threeInARow)
+        {   
+            let arr= [];
+            for(let index of combo)
+              arr.push(gameBoardArr[index])
+            
+            if(arr.join('')=== 'XXX' || arr.join('')=== 'OOO')
+                return 'WINNER';
+        }
+
+        if(!gameBoardArr.includes(undefined))
+            return 'TIE'
+        
+    }
 
     const arrDisplay= ()=>{
-        for(let i=0; i< gameBoardArr.length; i++)
+        for(let index in gameBoardArr)
         {
-            boxDivs[i].textContent= gameBoardArr[i];
-        };
+            boxDivs[index].textContent= gameBoardArr[index];
+        }
     }
 
 
     const input = ()=>{
+
         gameBoardDiv.addEventListener('click', (e)=>{
             let box= e.target;
             if(!box.textContent)
@@ -37,33 +70,47 @@ const gameBoard = (()=>{
                    arrDisplay();
                    //console.log(gameBoardArr)
             }
+            let result = winnerCheck();
+            if(result=== 'WINNER' || result==='TIE')
+                console.log(result)
         })
+
         clearBoardBtn.addEventListener('click', ()=>{
             boxDivs.forEach(box=> box.textContent='')
-            for(let i=0; i<9;i++)
-                gameBoardArr[i]=undefined;
+            for(let index in gameBoardArr)
+                gameBoardArr[index]=undefined;
             //console.log(gameBoardArr)
         })
     }
-    return {gameBoardArr, input,arrDisplay };
+    return {input,arrDisplay};
 })();
 
-console.log()
 
 
+const makePlayer = (name) =>{
 
-//displayController.arrDisplay(gameBoard.gameBoardArr)
-gameBoard.input();
-gameBoard.arrDisplay()
-
-const makePlayer = (name,score) =>{
-
-    const getScore = ()=> score;
-    const getName = ()=> name;
-    return {name, score};
+    return {name};
 }
 
 
-let nash = makePlayer('Nashit', 0);
-// console.log(gameBoard.gameBoardArr)
+let nash = makePlayer('Nashit');
 
+const playGame = ()=>{
+    let p1='';
+    let p2= '';
+    const btnsDiv= document.querySelector('.btns');
+    btnsDiv.addEventListener('change', (e)=>{
+        if(e.target.id=== 'pOne')
+            p1= makePlayer(e.target.value)
+        else if(e.target.id=== 'pTwo')
+            p2= makePlayer(e.target.value)
+        console.log(p1, p2);
+    }) 
+    
+    gameBoard.input();
+    gameBoard.arrDisplay()
+    
+}
+
+
+playGame();
